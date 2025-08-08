ee51f65fa1546d432e8953a59d832049cb9fc96a
Jhon Deivid Rojas Rodriguez <jhonrojasr253@gmail.com>
2025-08-08 07:02:07 -0500

    Creacion de textboxs y datagrid y botones

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            txtNombre = new TextBox();
+            txtDescripcion = new TextBox();
+            txtPrecio = new TextBox();
+            Crear = new Button();
+            dataGridView1 = new DataGridView();
+            Eliminar = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(67, 35);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(125, 27);
+            txtNombre.TabIndex = 0;
+            // 
+            // txtDescripcion
+            // 
+            txtDescripcion.Location = new Point(67, 89);
+            txtDescripcion.Name = "txtDescripcion";
+            txtDescripcion.Size = new Size(125, 27);
+            txtDescripcion.TabIndex = 1;
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.Location = new Point(67, 151);
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.Size = new Size(125, 27);
+            txtPrecio.TabIndex = 2;
+            // 
+            // Crear
+            // 
+            Crear.Location = new Point(60, 228);
+            Crear.Name = "Crear";
+            Crear.Size = new Size(142, 42);
+            Crear.TabIndex = 3;
+            Crear.Text = "Crear producto";
+            Crear.UseVisualStyleBackColor = true;
+            Crear.Click += Crear_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(338, 47);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(300, 188);
+            dataGridView1.TabIndex = 4;
+            // 
+            // Eliminar
+            // 
+            Eliminar.Location = new Point(338, 291);
+            Eliminar.Name = "Eliminar";
+            Eliminar.Size = new Size(142, 42);
+            Eliminar.TabIndex = 5;
+            Eliminar.Text = "Eliminar producto";
+            Eliminar.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(496, 291);
+            button2.Name = "button2";
+            button2.Size = new Size(142, 42);
+            button2.TabIndex = 6;
+            button2.Text = "Editar producto";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(Eliminar);
+            Controls.Add(dataGridView1);
+            Controls.Add(Crear);
+            Controls.Add(txtPrecio);
+            Controls.Add(txtDescripcion);
+            Controls.Add(txtNombre);
+            Name = "Form1";
+            Text = "Form";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtNombre;
+        private TextBox txtDescripcion;
+        private TextBox txtPrecio;
+        private Button Crear;
+        private DataGridView dataGridView1;
+        private Button Eliminar;
+        private Button button2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace PRODUCTOS
         {
             InitializeComponent();
         }
+
+        private void Crear_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
