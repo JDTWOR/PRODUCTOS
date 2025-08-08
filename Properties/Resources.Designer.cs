b36cad29f61ba1b6f2b9d6221c82d493f6b14728
jhon santamaria <jairoporrasapple@gmail.com>
2025-08-08 12:28:46 -0500

    nueva imagen

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace PRODUCTOS.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap fedora2 {
+            get {
+                object obj = ResourceManager.GetObject("fedora2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
