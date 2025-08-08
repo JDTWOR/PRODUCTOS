148816189e360a5307a22403cbd6c71843659db7
Jhon Deivid Rojas Rodriguez <jhonrojasr253@gmail.com>
2025-08-08 07:40:48 -0500

    Confirguracion db e insertar nuevo producto

--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,8 @@ namespace PRODUCTOS
 
 
     {
+        string rutaDB = "Server=localhost;Database=productos_csharp;User ID=root;Password=1234;";
+        private MySqlConnection conexion;
         public Form1()
         {
             InitializeComponent();
@@ -16,12 +18,39 @@ namespace PRODUCTOS
 
         private void Crear_Click(object sender, EventArgs e)
         {
+            if (txtNombre.Text == "" || txtDescripcion.Text == "" || txtPrecio.Text == "")
+            {
+                MessageBox.Show("Ingrese los datos completos");
+            }
 
+            try
+            {
+                string nombre = txtNombre.Text;
+                string descripcion = txtDescripcion.Text;
+                string pre = txtPrecio.Text;
+                double precio = Convert.ToDouble(pre);
+
+                conexion = new MySqlConnection(rutaDB);
+                conexion.Open();
+                string query = "INSERT INTO producto (nombre, descripcion, precio) VALUES (@nombre, @descripcion, @precio)";
+                MySqlCommand comando = new MySqlCommand(query, conexion);
+                comando.Parameters.AddWithValue("@nombre", nombre);
+                comando.Parameters.AddWithValue("@descripcion", descripcion);
+                comando.Parameters.AddWithValue("@precio", precio);
+                comando.ExecuteNonQuery();
+                conexion.Close();
+                MessageBox.Show("Producto creado exitosamente");
+
+            }
+            catch 
+            {
+                MessageBox.Show("Error al crear el producto. Verifique los datos ingresados.");
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string conexion = "Server=localhost;Database=producto;User ID=root;Password=;SslMode=none;";
+            string conexion = "Server=localhost;Database=productos_csharp;User ID=root;Password=1234;";
 
             using (MySqlConnection conn = new MySqlConnection(conexion))
             {
